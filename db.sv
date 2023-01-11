07b50305b2a9f48e282f5ab284dbac2cfbf53f2c
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-01-11 15:14:45 -0800

    Update db.sv

--- db.sv
+++ db.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/11/2022 07:12:31 PM
-// Design Name: 
-// Module Name: db
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module db(
     input logic clk, reset,
